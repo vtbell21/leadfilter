26d58c06d93b090b72bf398b11afa2f2ce93199b
Taylor Bell <taylorbell@Taylors-MacBook-Pro.local>
2025-04-27 02:04:24 -0600

    Add debug print for DATABASE_URL at top of settings.py

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+print("DEBUG: DATABASE_URL =", os.environ.get("DATABASE_URL"))
 DATABASES = {
     'default': dj_database_url.parse(
         os.environ.get('DATABASE_URL'),
