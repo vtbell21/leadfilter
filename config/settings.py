da2970b9397324c66473c7219efc90074a13a5e3
Taylor Bell <taylorbell@Taylors-MacBook-Pro.local>
2025-05-04 17:06:53 -0600

    Remove debug print for PIPEDRIVE_CLIENT_ID from settings.py

--- config/settings.py
+++ config/settings.py
@@ -202,5 +202,3 @@ NUMVERIFY_API_KEY = os.environ.get('NUMVERIFY_API_KEY')
 PIPEDRIVE_CLIENT_ID = os.getenv('PIPEDRIVE_CLIENT_ID')
 PIPEDRIVE_CLIENT_SECRET = os.getenv('PIPEDRIVE_CLIENT_SECRET')
 PIPEDRIVE_REDIRECT_URI = 'https://spamguardai.com/pipedrive/callback/'  # update for production
-
-print("PIPEDRIVE_CLIENT_ID:", os.getenv('PIPEDRIVE_CLIENT_ID'))
