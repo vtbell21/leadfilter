82e8b610f349f1652d9d14a23ffdd42efefc7bb5
Taylor Bell <taylorbell@Taylors-MacBook-Pro.local>
2025-05-10 13:36:02 -0600

    Remove local validate_phone_twilio, ensure import from leads.utils.phone_validation is used everywhere.

--- leads/views.py
+++ leads/views.py
@@ -29,7 +29,7 @@ from leads.models import FacebookLead
 from leads.services import salesforce, zoho, pipedrive, gohighlevel
 from django.db.models import Q
 from leads.hubspot_utils import create_hubspot_contact
-from leads.utils.phone_validation import validate_phone_twilio, normalize_phone_number
+from leads.utils.phone_validation import normalize_phone_number, validate_phone_twilio
 
 logger = logging.getLogger(__name__)
 
